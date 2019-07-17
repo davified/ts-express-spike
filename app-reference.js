a1fd3b7ac48cd2b4f99b87bd33a5faf04a43260f
David Tan <davidtan@thoughtworks.com>
2019-07-17 17:17:50 +0800

    Create hello world express app in ts

---
+++ app-reference.js
@@ -0,0 +1,11 @@
+// hello world in javascript (for comparison with app.ts)
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
