c31c0416ba89402b5b0e938aceb5a63370010915
David Tan <davidtan@thoughtworks.com>
2019-07-17 17:35:09 +0800

    Add instructions and config for linting typescript

---
+++ README.md
@@ -0,0 +1,13 @@
+# TypeScript Express Spike
+
+### Steps
+```sh
+### Linting
+# npm install tslint -D
+
+# Generate tslint config
+$(npm bin)/tslint --init
+
+# Run lint
+npm run tslint
+```
--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-import express = require('express');
+import express = require("express");
 
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
 });
