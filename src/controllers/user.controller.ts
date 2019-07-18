0a07819411fcd6c6ef8b62b473dcf9fec859afae
Emma Sun and Ioana Barcan <emma.sun+ioana.barcan@thoughtworks.com>
2019-07-18 14:52:37 +0800

    formatting file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,7 +6,10 @@ interface ICreateUser {
   about: IUser["about"];
 }
 
-const createUser = async ({ name, about="" }: ICreateUser): Promise<IUser> => {
+const createUser = async ({
+  name,
+  about = "",
+}: ICreateUser): Promise<IUser> => {
   const data: IUser = await User.create({
     name,
     about,
