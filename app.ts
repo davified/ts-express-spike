4351514aae606ebf4d0f0b382e5a2e7d808d1916
Emma Sun and Ioana Barcan <emma.sun+ioana.barcan@thoughtworks.com>
2019-07-18 14:22:53 +0800

    modularize user route

--- app.ts
+++ app.ts
@@ -1,65 +1,19 @@
-import asyncHandler from "express-async-handler";
 import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
-import userController from "./controllers/user.controller";
-import User from "./models/user.model";
+
+const UserRouter = require("./routes/user.routes");
 
 mongoose.connect("mongodb://localhost:27017/local_db", {
   useNewUrlParser: true,
 });
 
-// Create a new express application instance
 const app: express.Application = express();
 app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use("/users", UserRouter);
+app.get("/*", (req, res) => {
+  res.json({ message: "Hello World" });
 });
-
-app.get(
-  "/users",
-  asyncHandler(async (req, res) => {
-    const data = await User.find();
-    res.send(data);
-  })
-);
-
-app.get(
-  "/users/:name",
-  asyncHandler(async (req, res) => {
-    const name = req.params.name;
-    const data = await User.findOne({ name });
-    res.json(data);
-  })
-);
-
-app.put(
-  "/users/:name",
-  asyncHandler(async (req, res) => {
-    const name = req.params.name;
-    const data = await User.findOneAndUpdate({ name }, req.body, { new: true });
-    res.send(data);
-  })
-);
-
-app.delete(
-  "/users/:name",
-  asyncHandler(async (req, res) => {
-    const name = req.params.name;
-    const data = await User.remove({ name });
-    res.send(data);
-  })
-);
-
-app.post(
-  "/users",
-  asyncHandler(async (req, res) => {
-    const { name } = req.body;
-    const user = await userController.createUser({ name });
-    res.json(user);
-  })
-);
 
 app.listen(3000, () => {
   // tslint:disable-next-line:no-console
---
+++ routes/user.routes.ts
@@ -0,0 +1,51 @@
+import asyncHandler from "express-async-handler";
+import express from "express";
+import userController from "../controllers/user.controller";
+import User from "../models/user.model";
+
+const router = express.Router();
+
+router
+  .get(
+    "/",
+    asyncHandler(async (req, res) => {
+      const data = await User.find();
+      res.send(data);
+    })
+  )
+  .get(
+    "/:name",
+    asyncHandler(async (req, res) => {
+      const name = req.params.name;
+      const data = await User.findOne({ name });
+      res.json(data);
+    })
+  )
+  .post(
+    "/",
+    asyncHandler(async (req, res) => {
+      const { name } = req.body;
+      const user = await userController.createUser({ name });
+      res.json(user);
+    })
+  )
+  .put(
+    "/:name",
+    asyncHandler(async (req, res) => {
+      const name = req.params.name;
+      const data = await User.findOneAndUpdate({ name }, req.body, {
+        new: true,
+      });
+      res.send(data);
+    })
+  )
+  .delete(
+    "/:name",
+    asyncHandler(async (req, res) => {
+      const name = req.params.name;
+      const data = await User.remove({ name });
+      res.send(data);
+    })
+  );
+
+module.exports = router;
